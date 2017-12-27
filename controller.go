1ca4213c65ef434ea7351e3354ffa26eb1c2dda5
James Dunne <james.jdunne@gmail.com>
2017-12-26 21:28:19 -0600

    Stubbing out controller state structs.

--- controller.go
+++ controller.go
@@ -4,8 +4,29 @@ import (
 	"fmt"
 )
 
+type AmpMode int
+
+const (
+	AmpDirty AmpMode = iota
+	AmpClean
+	AmpAcoustic
+)
+
+type AmpState struct {
+	mode      AmpMode
+	dirtyGain uint8
+	cleanGain uint8
+}
+
+type ControllerState struct {
+	fsw FswState
+	amp [2]AmpState
+}
+
 type Controller struct {
 	fswCh <-chan FswState
+	curr  ControllerState
+	prev  ControllerState
 }
 
 func NewController(fswCh <-chan FswState) *Controller {
@@ -18,6 +39,7 @@ func (c *Controller) Loop() (err error) {
 	for {
 		select {
 		case state := <-c.fswCh:
+			c.curr.fsw = state
 			fmt.Printf("state = %v\n", state)
 			break
 		}
