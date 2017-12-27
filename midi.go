9d4ddcdee879bedf6a94965eafa3377937f2a60b
James Dunne <james.jdunne@gmail.com>
2017-12-27 15:07:19 -0600

    Implemented simple MIDI OUT driver for Linux.

--- midi.go
+++ midi.go
@@ -4,7 +4,10 @@ package main
 type Midi interface {
 	Close() error
 
+	// Controller change:
 	CC(channel uint8, controller uint8, value uint8) error
+	// Program change:
 	PC(channel uint8, program uint8) error
+	// send sysex data; F0 and F7 are automatically included
 	Sysex(data []byte) error
 }
--- midi_linux.go
+++ midi_linux.go
@@ -1,23 +1,43 @@
 // midi_linux
 package main
 
+import (
+	"os"
+)
+
 type midiImpl struct {
+	f *os.File
 }
 
 func NewMidi() (midi Midi, err error) {
-	midi = &midiImpl{}
+	// Open midi device:
+	var f *os.File
+	f, err = os.OpenFile("/dev/midi1", os.O_WRONLY, 0600)
+	if err != nil {
+		return
+	}
+
+	midi = &midiImpl{
+		f: f,
+	}
 	return
 }
 
 func (m *midiImpl) Close() error {
-	return nil
+	return m.f.Close()
 }
+
 func (m *midiImpl) CC(channel uint8, controller uint8, value uint8) error {
-	return nil
+	n, err := m.f.Write([]byte{0xB0 | channel, controller, value})
+	return err
 }
+
 func (m *midiImpl) PC(channel uint8, program uint8) error {
-	return nil
+	n, err := m.f.Write([]byte{0xC0 | channel, program})
+	return err
 }
+
 func (m *midiImpl) Sysex(data []byte) error {
-	return nil
+	n, err := m.f.Write([]byte{0xF0, ...data, 0xF7})
+	return err
 }
